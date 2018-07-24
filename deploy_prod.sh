9c79cdb9246afc4cbee5ebae91ffcf7d542bffc7
Megh-Thakkar <megh.1211@gmail.com>
2018-07-25 03:54:38 +0530

    Updated deploy command for sudo non-root users

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Use the following if user is root
 ansible-playbook ./prod/deploy.yml --private-key=<Path-to-ssh-keys> -K -u <Username> -i ./prod/hosts -vvv
+
+# Use the following for sudo non-root users
+ansible-playbook ./prod/deploy.yml --private-key=~/.ssh/id_rsa -K -u megh --extra-vars "ansible_sudo_pass=changeme" -i ./prod/hosts -vvv
